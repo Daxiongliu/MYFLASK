c753de91e3367aacdfe603d0ae2240cd0b00d733
daxiong <15612288557@163.com>
2017-05-09 14:52:59 +0800

    basic

--- config.py
+++ config.py
@@ -14,6 +14,8 @@ class Config:
     FLASK_MAIL_SUBJET_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     @staticmethod
     def init_app(app):
